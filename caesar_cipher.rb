a594c4af8c2773c9e998e76917dd045626eabf42
kevin Cotrina <kcotrinam92@gmail.com>
2020-10-06 15:20:00 -0500

    fix method for index letters higher than 25

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -5,18 +5,14 @@ def caesar_cipher(string, num)
 
   string.length.times do |i|
     if low_list.include?(string[i])
-      letter_index = low_list.index(string[i])
-      letter_index = - 1 if letter_index == 25
-      new_arr.push(low_list[letter_index + num])
+      letter_index = low_list.index(string[i]) + num - 25
+      new_arr.push(low_list[letter_index - 1])
     elsif up_list.include?(string[i])
-      letter_index = up_list.index(string[i])
-      letter_index = - 1 if letter_index == 25
-      new_arr.push(up_list[letter_index + num])
+      letter_index = up_list.index(string[i]) + num - 25
+      new_arr.push(up_list[letter_index - 1])
     else
       new_arr.push(string[i])
     end
   end
   new_arr.join('')
 end
-
-p caesar_cipher('y', 5)
